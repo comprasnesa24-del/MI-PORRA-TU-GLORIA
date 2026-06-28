eb8f67d0836d378424a8c92e15881fbc0a431308
comprasnesa24-del <comprasnesa24@gmail.com>
2026-06-28 12:47:04 +0200

    Ocultar podios hasta cierre de plazo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mi-porra-tu-gloria-v13';
+const CACHE_NAME='mi-porra-tu-gloria-v14';
 const FILES_TO_CACHE=['/','/index.html','/style.css','/src/main.js','/assets/hero-porra-gloria.png'];
 
 self.addEventListener('install',e=>{
