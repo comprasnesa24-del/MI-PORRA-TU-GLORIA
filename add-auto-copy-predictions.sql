539f96669875542fef42cbedb03d479440ea7a0f
comprasnesa24-del <comprasnesa24@gmail.com>
2026-08-12 10:15:38 +0200

    Añadir autocopia de pronosticos

---
+++ add-auto-copy-predictions.sql
@@ -0,0 +1,5 @@
+﻿-- Personal switch to copy predictions to the user's other pools.
+-- Non-destructive: disabled by default.
+
+alter table public.profiles
+  add column if not exists auto_copy_predictions boolean not null default false;
